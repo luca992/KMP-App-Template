ff8693eb50217f3442fc67a37f7467b35c9031c0
Luca Spinazzola <mspinluca@gmail.com>
2024-03-11 00:53:22 -0400

    remove unused

--- webApp/src/jsMain/kotlin/com/jetbrains/kmpapp/App.kt
+++ webApp/src/jsMain/kotlin/com/jetbrains/kmpapp/App.kt
@@ -2,16 +2,9 @@ package com.jetbrains.kmpapp
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import com.jetbrains.kmpapp.di.initKoin
 import com.jetbrains.kmpapp.screens.list.ListScreen
 
 @Composable
 fun App() {
     Navigator(ListScreen)
-}
-
-@Composable
-fun AppAndroidPreview() {
-    initKoin()
-    App()
 }

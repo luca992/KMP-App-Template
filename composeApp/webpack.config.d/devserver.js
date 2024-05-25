3964461a4580c121e93bfc558e487721d8220b5b
Luca Spinazzola <mspinluca@gmail.com>
2024-05-25 15:44:10 -0400

    fix dev-server for webpack 5

--- composeApp/webpack.config.d/devserver.js
+++ composeApp/webpack.config.d/devserver.js
@@ -1,10 +1,9 @@
 config.devServer = {
     ...config.devServer, // merges any locally defined server options
-    proxy: {
-        '/CRDImages': {
-            target: 'https://images.metmuseum.org',
-            changeOrigin: true, // Important for CORS
-            secure: false, // If you're proxying to a HTTPS domain, this may be needed
-        },
-    },
+    proxy: [{
+        context: ["/CRDImages"],
+        target: 'https://images.metmuseum.org',
+        changeOrigin: true, // Important for CORS
+        secure: false, // If you're proxying a HTTPS domain, this may be needed
+    }],
 }
